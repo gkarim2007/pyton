56d7363d9456af2d6b189b8272aa128789992cae
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-03-28 17:21:22 +0000

    sahrg

--- dzzzzzzzzzzzzz/pytondz.py
+++ dzzzzzzzzzzzzz/pytondz.py
@@ -1,9 +1,13 @@
-a=int(input())
-b=int(input())
-sum=1
 
-for i in range(b):
-    sum=sum*a
-        
-print(sum)
+a=(input())
 
+a=a.split(" ")
+
+if a[1]=="+":
+    print(a[0]+a[2])
+if a[1]=="-":
+    print(a[0]-a[2])
+if a[1]=="*":
+    print(a[0]*a[2])
+if a[1]=="/":
+    print(a[0]/a[2])
