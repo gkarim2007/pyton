a488869be8f683e58952269effffa8b14f95a580
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-03-30 18:30:38 +0000

    yrthyr

--- dzzzzzzzzzzzzz/pytondz1.py
+++ dzzzzzzzzzzzzz/pytondz1.py
@@ -1,5 +1,22 @@
-a=int(input())
-b=(input())
-
-for i in range(a):
-    print(b)
+a = input([1,0,-3,4])
+min=99
+max=0
+mi=0
+ma=0
+no=0
+for i in a:
+    if a[i]<0:
+        mi=+1
+    if a[i]>0:
+        ma=+1
+    if a[i]==0:
+        no=+1
+    if a[i]<min:
+        min=a[i]
+    if a[i]>max:
+        max=a[i]
+print(max)
+print(min)
+print(mi)
+print(ma)
+print(no)
