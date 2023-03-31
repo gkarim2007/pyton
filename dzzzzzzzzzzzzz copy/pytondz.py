9362ecfa0d71157b81fc0482e382fcd065f8600c
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-03-31 06:48:05 +0000

    rhtdsr

--- dzzzzzzzzzzzzz copy/pytondz.py
+++ dzzzzzzzzzzzzz copy/pytondz.py
@@ -1,11 +1,25 @@
-
-
-a=int(input())
-
-b=int(input())
-
-for i in range (a,b+1):
-    if i==3:
-        print(i)
-    if i%3!=0 and i%2!=0:
-        print(i)
+a = ([1,2,7,4])
+b = ([8,3,6,9])
+min=99
+max=0
+min2=99
+max2=0
+for i in a:
+    if a[i]<min:
+        min=a[i]
+    if a[i]>max:
+        max=a[i]
+for i in b:
+    if b[i]<min2:
+        min2=b[i]
+    if b[i]>max2:
+        max2=b[i]
+print(max)
+print(min)
+print(max2)
+print(min2)
+for i in a:
+    for i in b:
+        if b[i]<a[i]:
+            print(b)
+    
--- dzzzzzzzzzzzzz copy/pytondz2.py
+++
@@ -1,6 +0,0 @@
-a=int(input())
-
-b=int(input())
-for i in range(a, b+1):
-    for j in range(1, 10+1):
-        print (i,"*",j,"=",i*j)
