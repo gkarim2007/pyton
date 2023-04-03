51e438f01b93a7cda3b4725378db22e241723470
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-04-03 17:50:48 +0000

    dfyhbtfyhdtd

---
+++ dzzzzzzzzzzzzz copy 3/pytondz1.py
@@ -0,0 +1,7 @@
+a = [1,2,4,3,1,4,6,7]
+b=4
+c=0
+for i in range(len(a)):
+    if a[i]==b:
+        c=c+1
+print(c)
---
+++ dzzzzzzzzzzzzz copy 3/pytondz2.py
@@ -0,0 +1,7 @@
+a = [1,2,4,3,1,4,6,7]
+
+c=0
+for i in range(len(a)):
+    c=c+a[i]
+print(c/len(a))
+print(c)
