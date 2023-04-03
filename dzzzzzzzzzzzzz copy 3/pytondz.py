cf3c831bd285b12e6ce7eba40d803f01287e0725
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-04-03 15:51:12 +0000

    yhgyuhyk

--- dzzzzzzzzzzzzz copy 3/pytondz.py
+++ dzzzzzzzzzzzzz copy 3/pytondz.py
@@ -4,6 +4,12 @@ mi=0
 ch=0
 ne=0
 pr=1
+min=a[0]
+max=a[0]
+minmax=1
+poloj1=0
+poloj2=0
+poloj=0
 for i in range (size):
     if a[i]<0:
         mi=mi+a[i]
@@ -13,9 +19,22 @@ for i in range (size):
         ne=ne+a[i]
     if i%3==0:
         pr=pr*a[i]
-        
-    
+    if a[i]<min:
+        min=i
+    if a[i]>max:
+        max=i
+    if a[i]>0 and poloj1!=0:
+        poloj1=i
+    if a[i]>0:
+        poloj2=i
+
 print(mi)
 print(ch)
 print(ne)
 print(pr)
+for i in range (min - 1,max):
+    minmax=minmax*a[i]
+print(minmax)
+for i in range (poloj1-1,poloj2):
+    poloj=poloj+a[i]
+print(poloj)
