17db953f871bae3be745d38d23669ae921ad3d8a
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-04-06 18:09:45 +0000

    fhdgjfg

--- dzzzzzzzzzzzzz copy 3/pytondz1.py
+++ dzzzzzzzzzzzzz copy 3/pytondz1.py
@@ -1,7 +1,29 @@
-a = [1,2,4,3,1,4,6,7]
-b=4
+a = [1,2,-4,3,-1,-4,6,-7]
+b=[0,0,0,0,0,0,0,0,0]
 c=0
+d=[0,0,0,0,0,0,0,0,0]
+k=0
+y=[0,0,0,0,0,0,0,0,0]
+x=0
+l=[0,0,0,0,0,0,0,0,0]
+j=0
 for i in range(len(a)):
-    if a[i]==b:
-        c=c+1
-print(c)
+    if a[i]%2==0:
+        b[c]=a[i]
+        print (b[c])
+        c=+1
+for i in range(len(a)):
+    if a[i]%2!=0:
+        d[k]=a[i]
+        print (d[k])
+        k=+1
+for i in range(len(a)):
+    if a[i]>0:
+        y[x]=a[i]
+        print (y[x])
+        x=+1
+for i in range(len(a)):
+    if a[i]<0:
+        l[j]=a[i]
+        print (l[j])
+        j=+1
