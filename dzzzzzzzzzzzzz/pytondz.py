8d3693234f899fa014935b3906432251d93fe46f
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-05-19 16:32:05 +0000

    gygj

--- dzzzzzzzzzzzzz/pytondz.py
+++ dzzzzzzzzzzzzz/pytondz.py
@@ -1,9 +1,3 @@
-a=[4]
-a={1,3,2,5}
-cel=4
-for i in range(a.__len__):
-    for j in range((a.__len__)-1):
-        if a[i]+a[j]==cel:
-            print(i)
-            print(j)
-
+a="123"
+a=int(a)
+print(a+1)
